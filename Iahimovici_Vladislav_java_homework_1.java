fa6056538f7974fba5556119cbe76e2bfbd3287b
GhosTRideR03 <96689573+GhosTRideR03@users.noreply.github.com>
2022-10-14 02:53:10 +0300

    Add files via upload

---
+++ Iahimovici_Vladislav_java_homework_1.java
@@ -0,0 +1,43 @@
+package lesson.classwork;
+
+public class Iahimovici_Vladislav_java_homework_1 {
+    public static void main(String[] args) {
+        byte byteVal = 2;
+        short shortVal = 3;
+        int intVal = 10;
+        long longVal = 30;
+        System.out.println("Task 1:");
+        System.out.println(byteVal);
+        System.out.println(shortVal);
+        System.out.println(intVal);
+        System.out.println(longVal);
+
+        System.out.println("Task 2:");
+        System.out.println(54+16);
+
+        double divide = 50/3d;
+        System.out.println("Task 3:");
+        System.out.println(divide);
+
+        int multiply = 3;
+        System.out.println("Task 4:");
+        System.out.println(multiply * multiply);
+
+        int one = 0;
+        int two = 1;
+        int three = 2;
+        int four = 3;
+        int five = 4;
+        int six = 5;
+        int seven = 6;
+        int eight = 7;
+        int nine = 8;
+        int ten = 9;
+        int sum = (one + two + three + four + five + six + seven + eight + nine + ten) / 10;
+        System.out.println("Task 5:");
+        System.out.println(sum);
+
+
+
+    }
+}
